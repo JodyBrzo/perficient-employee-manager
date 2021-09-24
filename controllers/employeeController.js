1beb22816e1ea63c5e5c4c931f5157ebadef18f8
Jody Brzovski <jody.brzovski@gmail.com>
2021-09-24 12:39:06 -0400

    defining RESTful endpoints

---
+++ controllers/employeeController.js
@@ -0,0 +1,58 @@
+const db = require("../models");
+
+
+module.exports = {
+
+  //Get all Perficient employees.
+  findAllEmployees: function(req, res){
+
+  },
+
+  //Create a Perficient employee.
+  createEmployee: function(req, res){
+
+  },
+
+  //Find a Perficient employee by ID.
+  findEmployeeById: function(req, res){
+
+  },
+
+  //Update a Perficient employee by ID.
+  updateEmployeeById: function(req, res){
+
+  },
+
+  //Delete a Perficient employee by ID.
+  removeEmployeeById: function(req, res){
+
+  },
+
+  //Get all technical skills from a Perficient employee.
+  findAllSkillsByEmployee: function(req, res){
+
+  },
+
+  //Add a technical skill to a Perficient employee.
+  addSkillToEmployee: function(req, res){
+
+  },
+
+  //Find a technical skill, from a Perficient employee, by ID.
+  findSkillFromEmployeeById: function(req, res){
+
+  },
+
+  //Update a technical skill, from a Perficient employee, by ID.
+  updateSkillFromEmployeeById: function(req, res){
+
+  },
+
+  //Delete a technical skill, from a Perficient employee, by ID.
+  removeSkillFromEmployeeById: function(req, res){
+
+  },
+
+
+
+}
---
+++ routes/employee.js
@@ -0,0 +1,25 @@
+const router = require ("express").Router();
+const employeeController  = require("../../conrtoller/employeeController");
+
+router.route("/")
+.get(employeeController.findAllEmployees)  //Get all Perficient employees.
+
+router.route("/employees")
+.get(employeeController.findAllEmployees)  //Get all Perficient employees.
+.post(employeeController.createEmployee);  //Create a Perficient employee.
+
+router.route("/employees/:employeeId")
+.get(employeeController.findEmployeeById)  //Find a Perficient employee by ID.
+.put(employeeController.updateEmployeeById)  //Update a Perficient employee by ID.
+.delete(employeeController.removeEmployeeById);  //Delete a Perficient employee by ID.
+
+router.route("/employees/:employeeId/skills")
+.get(employeeController.findAllSkillsByEmployee)  //Get all technical skills from a Perficient employee.
+.post(employeeController.addSkillToEmployee);  //Add a technical skill to a Perficient employee.
+
+router.route("/:employeeId/skills/skillId")
+.get(employeeController.findSkillFromEmployeeById)  //Find a technical skill, from a Perficient employee, by ID.
+.put(employeeController.updateSkillFromEmployeeById)  //Update a technical skill, from a Perficient employee, by ID.
+.delete(employeeController.removeSkillFromEmployeeById);  //Delete a technical skill, from a Perficient employee, by ID.
+
+module.exports = router;
