bcadf90678e513158ed97d9e3f866114510b1bb4
Jody Brzovski <jody.brzovski@gmail.com>
2021-09-28 14:50:52 -0400

    displaying employee detail

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -3,9 +3,26 @@ import { Link, useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
 import API from "../utils/API";
+import Form from "react-bootstrap/Form";
+
 
 function Detail(props) {
-  const [] = useState({})
+  const [employeeState, setEmployee] = useState([]);
+  const {id} = useParams();
+  console.log("id:", id);
+  
+  useEffect(() =>{
+    console.log("id:", id);
+    loadEmployee();
+  }, [])
+
+  function loadEmployee() {
+    API.findEmployeeById(id) 
+      .then(res => {
+        setEmployee(res.data);
+      })
+      .catch(err => console.error(err))
+    };
 
   return (
       <Container fluid>
@@ -18,7 +35,57 @@ function Detail(props) {
         </Row>
         <Row>
           <Col size="md-12">
-
+            {console.log(employeeState)}
+            <Form>
+              <Form.Group className="mb-3" controlId="formFirstName">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter First Name" defaultValue={employeeState.firstName} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formLastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Last Name" defaultValue={employeeState.lastName}/>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formAddressStreet">
+                <Form.Label>Street</Form.Label>
+                <Form.Control type="text" placeholder="Enter Street" defaultValue={employeeState.address.street}/>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formAddressSuite">
+                <Form.Label>Suite</Form.Label>
+                <Form.Control type="text" placeholder="Enter Suite" defaultValue={employeeState.address.suite} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formAddressCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control type="text" placeholder="Enter City" defaultValue={employeeState.address.city} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formAddressRegion">
+                <Form.Label>Region</Form.Label>
+                <Form.Control type="text" placeholder="Enter Region" defaultValue={employeeState.address.region} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formAddressPostal">
+                <Form.Label>Postal Code</Form.Label>
+                <Form.Control type="text" placeholder="Enter Postal Code" defaultValue={employeeState.address.postal} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formAddressCountry">
+                <Form.Label>Country</Form.Label>
+                <Form.Control type="text" placeholder="Enter Country" defaultValue={employeeState.address.country} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formContactEmail">
+                <Form.Label>Contact Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter Contact Email" defaultValue={employeeState.contactEmail} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formCompanyEmail">
+                <Form.Label>Company Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter Company Email" defaultValue={employeeState.companyEmail} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBirthDate">
+                <Form.Label>Birth Date</Form.Label>
+                <Form.Control type="text" placeholder="Enter MM/DD/YYYY" defaultValue={employeeState.birthDate} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formHireDate">
+                <Form.Label>Hire Date</Form.Label>
+                <Form.Control type="text" placeholder="Enter MM/DD/YYYY" defaultValue={employeeState.hiredDate} />
+              </Form.Group>
+            </Form>          
           </Col>
         </Row>
         <Row>
